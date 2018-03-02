18b742a68ca6ba1789310daaa15fa99a6394ee77
Julien <jnlll72@gmail.com>
2018-03-02 14:06:57 +0100

    update planning

--- src/AppBundle/Resources/public/assets/js/script.js
+++ src/AppBundle/Resources/public/assets/js/script.js
@@ -8,11 +8,28 @@ $(function () {
 
     $(".space-event").droppable({
         drop: function (event, ui) {
+
             var elemDrop = ui.draggable;
+            var color = elemDrop.css("background-color");
+
             elemDrop.removeAttr("style");
             elemDrop.css({'position': 'relative'});
+            elemDrop.css({'background-color': color});
 
             $(this).append(elemDrop);
+
+            var hour = $(this).attr("data-space-event");
+            var day = $(this).parent().attr("data-day");
+            var id = elemDrop.attr("data-id");
+
+            var updateTime = new Date(day);
+            updateTime.setHours(hour);
+
+
+            $.post("http://127.0.0.1:8000/event/edit", {updateTime: updateTime, id: id}, function (data) {
+                console.log("OK");
+            });
+
         }
     });
 });
